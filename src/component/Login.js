407197d63bc1481706134fac912b8c13e51d8904
Baha Catherine Maigida <cathbaha1@gmail.com>
2022-11-07 12:03:07 +0100

    first commit

---
+++ src/component/Login.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import {useDispatch} from 'react-redux';
+import {login, logout} from '../features/user';
+function Login() {
+
+    const dispatch = useDispatch()
+  return (
+    <div>
+        <button onClick={() => {
+            dispatch(login({name: "Pedro", age: 20, email: "pedro@gmail.com"}));
+            }}>
+            login</button>
+
+            <button onClick={() => {
+            dispatch(logout());
+            }}>logout</button>
+      
+    </div>
+  );
+}
+
+export default Login;
